b431030404c4aaf87637f342bee23fdc7d5044af
Edward Huang <ewhuang3@timan107.cs.illinois.edu>
2017-12-04 20:38:01 -0600

    Added plotting fraction of agents documenting plot.

--- simulator.py
+++ simulator.py
@@ -12,8 +12,9 @@ np.random.seed(0)
 NUM_POINTS = 200 # Number of points in the graph.
 P = 0.05 # Probability of creating edge between two nodes in graph.
 MIN_STATE = -0.5 # Minimum state an agent can get to.
-RESOURCE = 'random' # Resource = 'random' or 1.
+RESOURCE = 1 # Resource = 'random' or 1.
 
+# Create results directory.
 DIR = './results/np_%d_p_%g_minstate_%g_res_%s' % (NUM_POINTS, P, MIN_STATE,
     RESOURCE)
 if not os.path.exists(DIR):
@@ -80,25 +81,39 @@ def plot_points(G, pos, i):
     '''
     Plots the graph.
     '''
-    plt.figure(figsize=(8,8))
+    plt.figure(figsize=(8, 8))
 
     nx.draw_networkx(G, node_size=100, with_labels=False, pos=pos,
         node_color=[n.state for n in G.nodes], cmap='Reds')
-    print [n.state for n in G.nodes]
 
     plt.axis('off')
     plt.savefig('%s/random_graph_%d.png' % (DIR, i))
-    plt.show()
+    plt.close()
+
+def get_fraction_documenting(G):
+    '''
+    Gets the fraction of agents who are documenting. Agents are documenting well
+    if their state is >= 0.9.
+    '''
+    node_lst = G.nodes
+    return len([n for n in node_lst if n.state >= 0.9]) / float(len(node_lst))
 
 def main():
-
     G = generate_points()
+
+    fraction_documenting_lst = []
 
     pos = nx.spring_layout(G)
     plot_points(G, pos, 0)
+    fraction_documenting_lst += [get_fraction_documenting(G)]
     for i in range(15):
         transform(G)
         plot_points(G, pos, i)
+        fraction_documenting_lst += [get_fraction_documenting(G)]
+
+    # Plot fraction of documenting agents at each iteration.
+    plt.plot(fraction_documenting_lst)
+    plt.savefig('%s/fraction_documenting.png' % DIR)
 
 if __name__ == '__main__':
     main()
