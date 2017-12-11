83cdf899c3f6bc7226e94a248b31634624cb843f
Cassandra J <csjacob2@illinois.edu>
2017-12-10 20:43:18 -0800

    * added type agents * needs some fine-tuning for the parameters

--- simulator.py
+++ simulator.py
@@ -9,22 +9,19 @@ import matplotlib.pyplot as plt
 np.random.seed(111)
 
 # Setting variables.
-NUM_POINTS = 200 # Number of points in the graph.
-NUM_EDGES = 5 # Number of edges from new node to existing nodes.
-RESOURCE = 1 # Resource = 'random' or 1.
-BETA = 0.5 # How much an agent discounts the utility of documentation.
+NUM_POINTS = 100 # Number of points in the graph.
+NUM_EDGES = 6 # Number of edges from new node to existing nodes.
+RESOURCE = 'random' # Resource = 'random' or 1.
+MAX_NUM_DOCS = 5 # Number of Documenters (maximum)
+
+BETA = 1 # How much an agent discounts the utility of documentation.
 DECAY = 0.1 # Fraction of documentation quality that decays each day.
-METHOD = 'nntu' # Perturbation method type.
-K = 40 # Top K. Only used for naive node degree methods.
 
 assert RESOURCE in [1, 'random']
-assert METHOD in ['none', 'nntu']
 
 # Create results directory.
-DIR = './results/np_%d_ne_%g_res_%s_b_%g_d_%g_m_%s' % (NUM_POINTS, NUM_EDGES,
-    RESOURCE, BETA, DECAY, METHOD)
-if METHOD != 'none':
-    DIR += '_k_%d' % K
+DIR = './results/np_%d_ne_%g_res_%s_mnd_%d_b_%g_d_%s' % (NUM_POINTS, NUM_EDGES,
+    RESOURCE, MAX_NUM_DOCS, BETA, DECAY)
 if not os.path.exists(DIR):
     os.makedirs(DIR)
 
@@ -34,22 +31,30 @@ class Agent(object):
     Attributes:
         label: An int specifying the node number.
         resource: A variable U(0, 1), the resource constraint of the agent.
+        type: A variable, Documenter (D), Engineer (E), or Combo (C), which determines the type of job and constrains the state
+            Documenter: only Documents, has state 1 always, has positive influence
+            Engineer: only Engineers/works, has state 0 always, can have positive or negative influence
+            Combo: works and documents, can have 1 or 0 state, can increase or decay and can have positive or negative influence but only on Engineers, not Documenters
         state: A float, determining how much the agent is documenting.
     """
 
-    def __init__(self, label):
+    def __init__(self, label, agentType):
         """Return an Agent object whose resource is *resource* and state is
         *state*."""
         self.label = label
+        self.agentType = agentType
 
         if RESOURCE == 1:
             self.resource = 1
         elif RESOURCE == 'random':
             self.resource = np.random.random_sample()
 
-        # State cannot exceed resource.
-        self.state = np.random.uniform(0, self.resource)
-
+        if self.agentType == 'D':
+            self.state = 1
+        else:
+            # State cannot exceed resource.
+            self.state = np.random.uniform(0, self.resource)
+    
     def set_state(self, new_state):
         # New state is at most the resource.
         # self.state = max(MIN_STATE, min(self.resource, self.state + new_state))
@@ -63,28 +68,59 @@ def generate_points():
 
     # Remap the nodes to Agent objects.
     mapping = {}
+
+    ''' let's assume one Documentor replaces 10 Engineers (we can adjust this)
+    assign the first Agent as a D, then the next DtoE Agents as Engineers, the rest as Combos
+    until we run out of Agents/nodes
+    we have a MAX_NUM_DOCS because a company may not have replacements for all workers
+    '''
+    ENG_TO_DOC = 10 #Engineer to Documentor ratio
+    DOCUMENTERS = 0
+    ENGINEERS = 0
+    TOTAL_DOCS = 0
+    
     for label in list(G.nodes):
-        mapping[label] = Agent(label)
+    
+        if DOCUMENTERS == 0 and MAX_NUM_DOCS != TOTAL_DOCS:
+            agentType = 'D'
+            DOCUMENTERS += 1
+            TOTAL_DOCS += 1
+        elif ENGINEERS != ENG_TO_DOC:
+            agentType = 'E'
+            ENGINEERS+=1
+        else:
+            agentType = 'C'
+
+        mapping[label] = Agent(label, agentType)
+    
+        # reset when we have to keep relabeling instead of filling up with Combo     
+        if ENGINEERS == ENG_TO_DOC and MAX_NUM_DOCS != TOTAL_DOCS:  
+            DOCUMENTERS = 0
+            ENGINEERS = 0
     return nx.relabel_nodes(G, mapping)
 
 def isWorking(node):
     '''
     The agent determines whether or not to work in this iteration.
-
         1. If the agent chooses to work, then it incurs a cost of 0.1. The
            benefit is BETA * 0.1, saving time in the future, but with present
            bias.
         2. If the agent chooses not to work, then it incurs a cost of its
            current progress * DECAY, since the documentation quality decays each
-           day its held off. The benefit is 0.1, since the agent gets that time
-           to do other things.
+           day its held off. The benefit is 0.1, since the agent continues to work
+           on their current tasks. Lazy utility is more desireable, but it will be influenced
+           by the social network in the transform function.
+    Documenters always return True (isWorking == isDocumenting)
     '''
+
     work_utility = BETA * 0.1 - 0.1
     lazy_utility = 0.1 - DECAY * node.state
+    print (str(work_utility) + ' > ' + str(lazy_utility))
 
-    if work_utility > lazy_utility:
+    if work_utility > lazy_utility or node.agentType == 'D':
         return True
     return False
+    
 
 def transform(G):
     '''
@@ -95,22 +131,22 @@ def transform(G):
     for node in G:
         working_status[node] = isWorking(node)
 
-    # Force nodes to alter their working states for different methods.
-    if METHOD == 'nntu':
-        # Here, force top k nodes with highest degree to work.
-        deg_lst = nx.classes.function.degree(G)
-        top_k = sorted(deg_lst, key=lambda x:x[1], reverse=True)[:K]
-        for node, degree in top_k:
-            working_status[node] = True
-
     for node in working_status:
-        neighbor_working_status = [working_status[n] for n in G.neighbors(node)]
-        # print neighbor_working_status
-        if working_status[node] or neighbor_working_status.count(True) >= 0.5 * len(neighbor_working_status):
-            node.set_state(node.state + 0.1)
-        else:
-            node.set_state((1 - DECAY) * node.state)
+        # can only change self working status if C type
+        #get working neighbour status in an array
 
+        neighbor_working_status = [working_status[n] for n in G.neighbors(node)]
+        neighbor_agent_type = [n.agentType for n in G.neighbors(node)]
+
+        if node.agentType == 'C' or node.agentType == 'E':
+            if neighbor_working_status.count(True) >= 0.5 * len(neighbor_working_status) or (neighbor_agent_type.count('D') >= 1):
+                #Documenters and working neighbours (E or C) directly influence positively to work
+                node.set_state(node.state + 0.1)
+            else:
+                #no neighbours or Ds working nearby, contributes to decays
+                node.set_state(node.state - DECAY)
+        
+                
 def plot_points(G, pos, i):
     '''
     Plots the graph.
@@ -119,8 +155,7 @@ def plot_points(G, pos, i):
     cmap = matplotlib.cm.Reds
     m = matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap)
 
-    nx.draw_networkx(G, node_size=100, with_labels=False, pos=pos,
-        node_color=[m.to_rgba(n.state) for n in G.nodes])
+    nx.draw_networkx(G, node_size=100, with_labels=False, pos=pos, node_color=[m.to_rgba(n.state) for n in G.nodes])
 
     plt.axis('off')
     plt.savefig('%s/random_graph_%d.png' % (DIR, i))
@@ -136,16 +171,16 @@ def get_fraction_documenting(G):
 
 def main():
     G = generate_points()
-
     fraction_documenting_lst = []
-
     # Get the layout of the graph.
     pos = nx.spring_layout(G)
 
     for i in range(15):
         plot_points(G, pos, i)
+
         fraction_documenting_lst += [get_fraction_documenting(G)]
 
+        print ('iteration ' + str(i))
         transform(G)
         # print [n.state for n in G.nodes()][:5]
 
